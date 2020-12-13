753fd03a87f700a159ace66a0e3e2f15d9ef6e37
Nathalia Costa <ghost2888@icloud.com>
2020-12-13 22:47:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,15 +15,21 @@ end
 
 
 def take_a_number(katz_deli, name)
-  katz_deli << "#{name}" 
+  katz_deli << name 
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
-end
+end 
+
+#   $counter = 0
+# def take_a_number(katz_deli)
+#   $counter += 1
+#   katz_deli << $counter
+#   puts "Welcome you are number #{$counter}"
+# end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}." 
-  end 
-  katz_deli.shift
+    puts "Currently serving #{katz_deli.shift}." 
+  end
 end
